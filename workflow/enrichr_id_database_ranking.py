631ee7c81e05e9019ecc01e8b75fdbe0acef0fd7
Potari-Gul <donkey-1818@hotmail.com>
2025-02-26 14:29:19 +0000

    Updated script to fix analysis level issue

--- workflow/enrichr_id_database_ranking.py
+++ workflow/enrichr_id_database_ranking.py
@@ -8,10 +8,12 @@ import numpy as np
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Your script description here.")
     parser.add_argument("--background_gene_list", required=True, help="Path to the file background gene list if applicable.")
+    parser.add_argument("--sep", required=True, help="Separator for the background gene list file")
     parser.add_argument("--target_gene_list", required=True, help="Path to the target gene list file.")
+    parser.add_argument("--analysis_level", required=False, help="Type of target gene set analysis. HMI option anlyses proteins targeted by bacterial interactions. TieDIE option focuses on the downstream signaling network")
     parser.add_argument("--output_image", required=True, help="Path to the output image.")
     parser.add_argument("--output_file", required=True, help="Path to the output file.")
-    parser.add_argument("--database", default="Reactome_2022", help="""Database to use as a reference for the enrichment analysis. 
+    parser.add_argument("--database", default="Reactome_2022", help="""Database to use as a reference for the enrichment analysis.
                                 Database to use as reference for the enrichment analysis.
                         Supported shortcuts (and their default database):
                         'pathway' (KEGG_2021_Human)
@@ -22,21 +24,30 @@ def parse_arguments():
                         'kinase_interactions' (KEA_2015)
                         or any database listed under Gene-set Library at: https://maayanlab.cloud/Enrichr/#libraries""")
     parser.add_argument("--ranking", default="combined_score", help="Plot will be ranked based on \"combined_score\" or \"adj_p\".")
-                                
-                                
+
+
     return parser.parse_args()
 
 #Read the background gene list from a file.
-def read_background_gene_list(file_path):
+def read_background_gene_list(file_path, sep):
     background_genes = []
     with open(file_path, "r") as background_gene_list:
         for line in background_gene_list:
-            line = line.strip().split(',')
+            line = line.strip().replace('\"', "").split(sep)
             background_genes.append(line[0])
     return background_genes
 
 #Read the target gene list from a file.
-def read_target_gene_list(file_path):
+def read_target_gene_list_HMI(file_path):
+    target_geneset = []
+    with open(file_path, "r") as target_gene_list:
+        for line in target_gene_list:
+            line = line.strip().split("\t")
+            target_geneset.extend([line[0], line[5]])
+    target_geneset = list(set(target_geneset))
+    return target_geneset
+
+def read_target_gene_list_TieDIE(file_path):
     target_geneset = []
     with open(file_path, "r") as target_gene_list:
         for line in target_gene_list:
@@ -49,15 +60,20 @@ def read_target_gene_list(file_path):
 def translate_uniprot_to_symbols(uniprot, species='human'):
     mg = MyGeneInfo()
     target_genesymbols_translation = mg.querymany(uniprot, scopes='uniprot', fields='symbol', species='human', returnall=True)
-    
+
     translation_dict = {entry['query']: entry.get('symbol', None) for entry in target_genesymbols_translation['out']}
     return translation_dict
 
 def main():
     args = parse_arguments()
 
-    background_expressed_gene_symbols = read_background_gene_list(args.background_gene_list)
-    target_geneset = read_target_gene_list(args.target_gene_list)
+    background_expressed_gene_symbols = read_background_gene_list(args.background_gene_list, args.sep)
+
+    if args.analysis_level == 'HMI':
+        target_geneset = read_target_gene_list_HMI(args.target_gene_list)
+
+    if args.analysis_level == 'TieDIE':
+        target_geneset = read_target_gene_list_TieDIE(args.target_gene_list)
 
     target_genesymbols = translate_uniprot_to_symbols(target_geneset)
     only_target_genesymbols = list(target_genesymbols.values())
@@ -71,13 +87,13 @@ def main():
 
         #Writing output file.
         filtered_enrichr_df_bkg.to_csv(args.output_file, index=False)
-    
+
         #Create bar plot ranked by combined score
         filtered_enrichr_df_bkg = filtered_enrichr_df_bkg.sort_values('combined_score', ascending = False)
         filtered_enrichr_inf = filtered_enrichr_df_bkg[filtered_enrichr_df_bkg['combined_score'].astype(str) != 'inf']
-    
+
         top_20_functions = filtered_enrichr_inf.head(20).sort_values('combined_score')
-    
+
         fig, ax1 = plt.subplots()
 
         ax1.barh(y= top_20_functions['path_name'], width= top_20_functions['combined_score'])
@@ -95,8 +111,8 @@ def main():
     elif args.ranking == "adj_p":
 
         enrichr_df_bkg = gget.enrichr(only_target_genesymbols, database=args.database, background_list=background_expressed_gene_symbols, plot=True)
-                
-        #Filtering results based on adjusted p-values.        
+
+        #Filtering results based on adjusted p-values.
         filtered_enrichr_df_bkg = enrichr_df_bkg[enrichr_df_bkg["adj_p_val"] < 0.05]
 
         #Writing output file.
